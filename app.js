f9c6cd30e60532c66e017cd5732cd751ed61be00
lastnamearya <arya.jigyasu6815@gmail.com>
2019-04-18 15:38:27 +0530

    Maximum Call Stack ~ too much recursion example

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// Write a Program to find the Exponent Power of a Number
-
-function power(base, exponent) {
-  let result = 1;
-  for (let i = 0; i < exponent; i++) {
-    result = result * base;
-  }
-  return result;
+function chicken() {
+  return egg();
 }
 
-console.log(power(2, 10));
+function egg() {
+  return chicken();
+}
+
+console.log(chicken() + " came first.");
