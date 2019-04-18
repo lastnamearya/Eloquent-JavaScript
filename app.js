ee071fb9d5c723c5aa94df6604f715c526cb5442
lastnamearya <arya.jigyasu6815@gmail.com>
2019-04-19 00:20:04 +0530

    Recursion ~ Side wise Side

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-// To the Power ~ With Recursion
+// Recursion Examples
+
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(5));
 
 function power(base, exponent) {
+  // When Exponent becomes 0 then it'll return 1 so that when base multiplied to returned value it won't be affect the final output.
   if (exponent === 0) {
     return 1;
   } else {
